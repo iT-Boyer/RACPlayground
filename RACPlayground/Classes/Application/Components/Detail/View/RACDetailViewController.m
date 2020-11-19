5e27bbbe9e151c4232f1ab71d015947bdedd3d7e
iTBoyer <iTBoyer@qq.com>
2020-11-19 19:06:44 +0800

    验证水印方法

--- RACPlayground/Classes/Application/Components/Detail/View/RACDetailViewController.m
+++ RACPlayground/Classes/Application/Components/Detail/View/RACDetailViewController.m
@@ -44,10 +44,11 @@
         make.center.equalTo(@0);
         make.size.mas_equalTo(CGSizeMake(400, 400));
     }];
-    NSString *imgUrl = @"https://images.dog.ceo/breeds/hound-afghan/n02088094_3630.jpg";
+    NSString *imgUrl = @"https://fileserver.iuoooo.com/Jinher.JAP.BaseApp.FileServer.UI/FileManage/GetFile?fileURL=29e54e46-3e17-4ca4-8f03-db71fb8f967d/TempDirectory/42034a45-037d-444e-b375-32d5679c3799_05949A4A-71D4-40F8-ABAC-1568E363EA84.png";
+    self.imgView.contentMode = UIViewContentModeScaleAspectFit;
     [self.imgView sd_setImageWithURL:[NSURL URLWithString:imgUrl] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
         NSString *userAccount = [self protectAccount:@"13522834108"];
-        NSAttributedString *attr = [[NSAttributedString alloc] initWithString:userAccount attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:24.0f],NSForegroundColorAttributeName:[UIColor whiteColor]}];
+        NSAttributedString *attr = [[NSAttributedString alloc] initWithString:userAccount attributes:@{NSFontAttributeName:[UIFont systemFontOfSize:44.0f],NSForegroundColorAttributeName:[UIColor whiteColor]}];
         UIImage *water = [image addWaterByPattern:attr];
         self.imgView.image = water;
     }];
@@ -70,17 +71,17 @@
     //XF_C_(self.btn, DataDriver, Command)
     
     // load or reset expressPack
-//    XF_Define_Weak
-//    [RACObserve(self.dataDriver, expressData) subscribeNext:^(NSString *imgUrl) {
-//        XF_Define_Strong
-//        // 如果有显示数据加载完成
-//        if (imgUrl) {
-//            [self.imgView sd_setImageWithURL:[NSURL URLWithString:imgUrl] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
-//                UIImage *water = [image addWaterByPattern:@"192333****4"];
-//                self.imgView.image = water;
-//            }];
-//        }
-//    }];
+    XF_Define_Weak
+    [RACObserve(self.dataDriver, expressData) subscribeNext:^(NSString *imgUrl) {
+        XF_Define_Strong
+        // 如果有显示数据加载完成
+        if (imgUrl) {
+            [self.imgView sd_setImageWithURL:[NSURL URLWithString:imgUrl] completed:^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+                UIImage *water = [image addWaterByPattern:@"192333****4"];
+                self.imgView.image = water;
+            }];
+        }
+    }];
 }
 
 
--- RACPlayground/Classes/Public/Category/JHGraphicsExtension/UIImage+Util.m
+++ RACPlayground/Classes/Public/Category/JHGraphicsExtension/UIImage+Util.m
@@ -306,17 +306,28 @@
     /**
      这里之所以外面再放一个UIView，是因为直接用label画图的话，旋转就不起作用了
      */
-    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 150, 150)];
+    CGFloat textW = 260;
+    CGFloat djx = textW + 60;
+    CGFloat textH = sqrt(djx*djx - textW*textW);//开方
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0, 0, textW, textH)];
     view.backgroundColor = [UIColor clearColor];
     
     UILabel *label = [[UILabel alloc] initWithFrame:view.bounds];
     label.backgroundColor = [UIColor clearColor];
+    label.textAlignment = NSTextAlignmentCenter;
     label.attributedText = text;
     label.transform = CGAffineTransformMakeRotation(-M_PI/4.0);
     [view addSubview:label];
     
-    UIGraphicsBeginImageContextWithOptions(view.bounds.size, NO, [UIScreen mainScreen].scale);
-    [view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    CGSize scaleSize = CGSizeMake(textW, textH);
+    UIGraphicsBeginImageContextWithOptions(scaleSize, NO, [UIScreen mainScreen].scale);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    [view.layer renderInContext:context];
+    //填充背景色
+//    CGContextSetFillColorWithColor(context, [[UIColor blueColor] CGColor]);
+//    CGContextFillRect(context, CGRectMake(0, 0, textW, textH));
+    
     UIImage *image=UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
 
@@ -324,15 +335,6 @@
 }
 // 给图片添加文字水印：
 - (UIImage *)waterImageWithSize:(CGSize)size text:(NSString *)text attr:(NSDictionary *)attr{
-//    UIGraphicsBeginImageContext(size);
-//    CGContextRef context = UIGraphicsGetCurrentContext();
-//
-////    CGContextSetFillColorWithColor(context, [color CGColor]);
-////    CGContextFillRect(context, rect);
-//
-//    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
-//    UIGraphicsEndImageContext();
-    
     //1.开启上下文
     UIGraphicsBeginImageContextWithOptions(size, NO, 0);
     //2.绘制图片
@@ -355,7 +357,7 @@
     [image2 drawInRect:CGRectMake(0, 0, image2.size.width, image2.size.height)];
     
     //Draw image1
-    [image1 drawInRect:CGRectMake(0, 0, image1.size.width, image1.size.height)];
+    [image1 drawInRect:CGRectMake(-20, 0, image1.size.width, image1.size.height)];
     
     UIImage *resultImage = UIGraphicsGetImageFromCurrentImageContext();
     
